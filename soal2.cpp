559892695aaa7fdaefaf7751f09d588a8cf457c3
Naufal <2500018043@webmail.uad.ac.id>
2025-11-05 11:53:51 +0700

    Soal UK 2

--- soal2.cpp
+++ soal2.cpp
@@ -2,25 +2,35 @@
 using namespace std;
 
 int main(){
-    int totalDetik;
+    int d;
+    char ulang;
+
+    do {
     cout << "Masukkan waktu dalam detik: ";
-    cin >> totalDetik;
+    cin >> d;
 
     int hari;
     int jam;
     int menit;
     int detik;
 
-    hari = totalDetik / 86400; //satu hari = 86400 detik
-    jam = (totalDetik % 86400) / 3600; //satu jam = 3600  detik, sisa hasil bagi (%) dari hari kemudian dibagi 3600 detik.
-    menit = (totalDetik % 3600) / 60; //satu menit = 60 detik, sisa hasil bagi (%) dari jam kemudian dibagi 60 detik.
-    detik = totalDetik % 60;
+    hari = d / 86400; //satu hari = 86400 detik
+    jam = (d % 86400) / 3600; //satu jam = 3600  detik, sisa hasil bagi (%) dari hari kemudian dibagi 3600 detik.
+    menit = (d % 3600) / 60; //satu menit = 60 detik, sisa hasil bagi (%) dari jam kemudian dibagi 60 detik.
+    detik = d % 60; 
 
-    cout << totalDetik << " detik = "; 
+    cout << d << " detik = "; 
     cout << hari << " Hari, "; 
     cout << jam << " Jam, "; 
     cout << menit << " Menit, "; 
     cout << detik << " Detik" << endl;
 
+    cout << "Apakah anda ingin konversi ulang? (y/n) : ";
+    cin >> ulang;
+
+    } while (ulang == 'y' || ulang == 'Y');
+
+    cout << "Program Selesai." << endl;
+    
     return 0;
 }
